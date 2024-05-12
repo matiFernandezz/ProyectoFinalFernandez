c5d72085384bdc7beba98da27643aacec0a12036
matiFernandez <matutefernandez20@gmail.com>
2024-05-12 17:53:10 -0300

    Modificando Logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar() {
    <>
       <nav className=" items-center justify-between gap-4 fixed-top hidden md:flex bg-orange-300">
             <Link to="/">
-              <img src="./src/multimedia/logo.png" alt="Foto Logo" width={60}/>
+              <img src="../../public/logo.png" alt="Foto Logo" width={60}/>
             </Link>
             <div className="items-center hidden md:flex md:gap-4">
               <NavLink to="/"  className="nav-link colorFuente">Productos</NavLink>
@@ -24,7 +24,7 @@ function NavBar() {
       </nav>
       <nav className="md:hidden navbar navbar-orange bg-orange-300 fixed-top">
         <div className="container-fluid flex justify-between">
-          <Link to="/"> <img src="./src/multimedia/logo.png" alt="Foto Logo" width={60}/></Link>
+          <Link to="/"> <img src="../../public/logo.png" alt="Foto Logo" width={60}/></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
           </button>
@@ -33,7 +33,7 @@ function NavBar() {
                 <button type="button" class="btn-close btn-close-dark" data-bs-dismiss="offcanvas" ></button>
              </div>
              <div className="offcanvas-body navbar-nav justify-content-start items-center flex-grow-1 pe-3">
-              <Link to="/"> <img src="./src/multimedia/logo.png" alt="Foto Logo" width={120}/></Link>
+              <Link to="/"> <img src="../../public/logo.png" alt="Foto Logo" width={120}/></Link>
               <NavLink to="/"  className="nav-link colorFuente">Productos</NavLink>
               <NavLink to={"/categoria/Procesador"} className="nav-link colorFuente" >Procesadores</NavLink>
               <NavLink to={"/categoria/Monitor"} className="nav-link colorFuente" >Monitores</NavLink>
